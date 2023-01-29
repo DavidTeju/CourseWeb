76af4747310184d890931ee2e5bd217df7fa25ff
David Tejuosho <tejuoshodavid@gmail.com>
2023-01-29 11:02:13 -0600

    fixed some file discrepancies

--- Scraper/courseType.ts
+++
@@ -1,9 +0,0 @@
-export type course = {
-    courseName: string;
-    courseCode: string;
-    courseLink: string;
-    preRequisites: string[];
-    aliases: string[];
-    satisfiedRequirements: string[];
-    description: string;
-};
